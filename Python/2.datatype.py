b3e84bda1468e9b851b583b7142b0ee38e148c74
diproutetree <102115427+diproutetree@users.noreply.github.com>
2022-03-29 14:21:45 +0900

    파이썬 실습(22.3.29)

---
+++ Python/2.datatype.py
@@ -0,0 +1,28 @@
+print(1)
+print(1.1)
+print(1+1)
+print(2*1)
+print(2-1)
+print(6/2)
+print(pow(4,3))
+import random
+print(random.random())
+
+#string
+print('Hello')
+print("Hello")
+print("""
+    Hello World
+    """)
+print(len('Hello'))
+
+#List 정리정돈이 된다. 수많은 연산들을 쉽게 만들어낼수 있다. 
+member = ['egoing','duru','taeho']
+print(member[0])
+print(len(member))
+
+import random
+print(random.choice(member))
+
+score = [100,200,300]
+print(sum(score))
---
+++ Python/7.conditionalapp.py
@@ -0,0 +1,9 @@
+input_id = input('아이디를 입력해주세요.')
+input_pwd = input('비밀번호를 입력해주세요.')
+if input_id == '강병준' :
+    if input_pwd == '11111':
+        print('안녕하세요')
+    else: 
+        print('비밀번호가 다릅니다.')
+else :
+    print('뉘슈?')
